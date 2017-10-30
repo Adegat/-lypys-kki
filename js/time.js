cee9f9d8c93b233bc13657ccda52dc953d693256
Adegat <adel.gatoui@gmail.com>
2017-10-30 14:58:48 +0200

    kelloa

--- js/time.js
+++ js/time.js
@@ -1,5 +1,20 @@
 
-now = new Date();
+/*now = new Date();
 localtime = now.toString();
 utctime = now.toGMTString();
-document.write(localtime + "</p>");
+document.write(localtime + "</p>");*/
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML =
+        h + ":" + m ;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
