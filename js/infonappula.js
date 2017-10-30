5c35931302f43ef950eb8d9fe85006b1eac0f3b2
jade <jasmiinv@metropolia.fi>
2017-10-30 14:59:52 +0200

    nappula kokoruudulla

--- js/infonappula.js
+++ js/infonappula.js
@@ -1,5 +1,16 @@
 
-function showbigmap()
+function isomap()
 {
+
+    var elem = document.getElementById("map");
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+        elem.msRequestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+        elem.webkitRequestFullscreen();
+    }
 
 }
