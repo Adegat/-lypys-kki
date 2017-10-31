75a41d062b11b6800a9c94d1931c565110e0bc23
jade <jasmiinv@metropolia.fi>
2017-10-31 14:43:47 +0200

    aikakokeilu

---
+++ js/aikataulu.js
@@ -0,0 +1,13 @@
+funtion getTimetables ()
+{
+    var times = [
+        "12:00",
+        "12:03",
+        "12:06",
+        "12:09"
+    ];
+    times.forEach(function (element) {
+
+    });
+
+}
--- js/gmap-fullscreen.js
+++ js/gmap-fullscreen.js
@@ -1,18 +1,3 @@
-function isomap(){
-
-buttonZoom.onclick = function() {
-    var div = document.getElementById("map");
-
-    if(div) {
-
-        div.style.height = "2900px";
-        div.style.width = "3200px";
-
-        return false;
-    }
-};
-
-}
 
 /*function isomap()
 {
@@ -28,82 +13,10 @@ buttonZoom.onclick = function() {
         elem.webkitRequestFullscreen();
     }
 
-}/*
-$(function() {
-    var map = new google.maps.Map(document.getElementById("map_canvas"), {});
-    var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+}*/
+function fullscreen() {
 
-    var googleMapWidth = $("#map_canvas").css('width');
-    var googleMapHeight = $("#map_canvas").css('height');
-
-    map.setCenter(newyork);
-
-    $('#enter-full-screen').click(function(){
-        $("#map_canvas").css("position", 'fixed').
-        css('top', 0).
-        css('left', 0).
-        css("width", '100%').
-        css("height", '100%');
-        google.maps.event.trigger(map, 'resize');
-        map.setCenter(newyork);
-        return false;
+    $("#map_toggler").click(function () {
+        $("#map").toggleClass("fullscreen")
     });
-
-    $('#exit-full-screen').click(function(){
-        $("#map_canvas").css("position", 'relative').
-        css('top', 0).
-        css("width", googleMapWidth).
-        css("height", googleMapHeight);
-        google.maps.event.trigger(map, 'resize');
-        map.setCenter(newyork);
-        return false;
-    });
-});
-
-var googleMapWidth = $("#map-container").css('width');
-var googleMapHeight = $("#map-container").css('height');
-
-$('#btn-enter-full-screen').click(function() {
-
-    $("#map-container").css({
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        backgroundColor: 'white'
-    });
-
-    $("#map-canvas").css({
-        height: '100%'
-    });
-
-    google.maps.event.trigger(map, 'resize');
-    map.setCenter(newyork);
-
-    // Gui
-    $('#btn-enter-full-screen').toggle();
-    $('#btn-exit-full-screen').toggle();
-
-    return false;
-});
-
-$('#btn-exit-full-screen').click(function() {
-
-    $("#map-container").css({
-        position: 'relative',
-        top: 0,
-        width: googleMapWidth,
-        height: googleMapHeight,
-        backgroundColor: 'transparent'
-    });
-
-    google.maps.event.trigger(map, 'resize');
-    map.setCenter(newyork);
-
-    // Gui
-    $('#btn-enter-full-screen').toggle();
-    $('#btn-exit-full-screen').toggle();
-    return false;
-});
-*/
+}
