0a8fb69b3f8fc4171d08fda19d3d0fef67afa10a
Adegat <adel.gatoui@gmail.com>
2017-11-09 12:18:56 +0200

    Kartalle reitti

--- js/map.js
+++ js/map.js
@@ -2,10 +2,11 @@
 * Luo google maps elementin ja hake kartan keskityksen locaatiao tietojen perusteella
 * */
 function initMap() {
-    var uluru = {lat: 60.2208061, lng: 24.8053184};
+    var uluru = {lat: 60.180700, lng: 24.831451};
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 18,
+        zoom: 16,
         center: uluru,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
         zoomControl: false,
         mapTypeControl: false,
         scaleControl: false,
@@ -13,31 +14,96 @@ function initMap() {
         rotateControl: false,
         fullscreenControl: false
     });
+  /*  var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+*/
+
+    /*    var infoWindow = new google.maps.InfoWindow;
+     /*   // Try HTML5 geolocation.
+       if (navigator.geolocation) {
+           navigator.geolocation.getCurrentPosition(function(position) {
+               var pos = {lat: position.coords.latitude, lng: position.coords.longitude};
+               var coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+               var marker = new google.maps.Marker({
+                   position: coords,
+                   map: map
+               });
+               infoWindow.setPosition(pos);
+               map.setCenter(pos);
+           }, function() {
+               handleLocationError(true, infoWindow, map.getCenter());
+           });
+       } else {
+           // Browser doesn't support Geolocation
+           handleLocationError(false, infoWindow, map.getCenter());
+       }*/
 
 
-    var infoWindow = new google.maps.InfoWindow;
 
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {lat: position.coords.latitude, lng: position.coords.longitude};
-            var coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-            var marker = new google.maps.Marker({
-                position: coords,
-                map: map
+
+
+/* Reitin piirto*/
+
+    var geocoder;
+    var directionsDisplay;
+    var directionsService = new google.maps.DirectionsService();
+    var locations = [
+        ['Manly Beach',60.180700, 24.831451, 1],
+        ['Bondi Beach',60.181969, 24.830747, 2],
+        ['Coogee Beach',60.183031, 24.829004, 3],
+         /*['Maroubra Beach', -33.950198, 151.259302, 1],
+        ['Cronulla Beach', -34.028249, 151.157507, 3]*/
+    ];
+
+        directionsDisplay = new google.maps.DirectionsRenderer();
+        directionsDisplay.setMap(map);
+        var infowindow = new google.maps.InfoWindow();
+        var marker, i;
+        var request = {
+            travelMode: google.maps.TravelMode.DRIVING
+        };
+        for (i = 0; i < locations.length; i++) {
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(locations[i][1], locations[i][2]),
             });
-            infoWindow.setPosition(pos);
-            map.setCenter(pos);
-        }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
+
+            google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
+                    infowindow.setContent(locations[i][0]);
+                    infowindow.open(map, marker);
+                }
+            })(marker, i));
+
+            if (i == 0) request.origin = marker.getPosition();
+            else if (i == locations.length - 1) request.destination = marker.getPosition();
+            else {
+                if (!request.waypoints) request.waypoints = [];
+                request.waypoints.push({
+                    location: marker.getPosition(),
+                    stopover: true
+                });
+            }
+
+        }
+        directionsService.route(request, function(result, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(result);
+            }
         });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
+
+
+
+
+
+
+
+
+
 
 }
-
+/*
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(browserHasGeolocation ?
@@ -45,3 +111,4 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         'Error: Your browser doesn\'t support geolocation.');
     infoWindow.open(map);
 }
+*/
