220a415705823e77d8be728298381747a446d618
Adegat <adel.gatoui@gmail.com>
2017-11-23 13:17:06 +0200

    info tekstit

--- js/info2.js
+++ js/info2.js
@@ -3,9 +3,11 @@ $.ajax({
     header: ('Content-Type: application/json; charset=utf-8'),
     method: 'GET'
 }).then(function(data) {
-    console.log(data);
+    linebreak= document.createElement("br")
+    console.log(data,'moi');
         //console.log("danger");
-        //document.querySelectorAll(".notices")[0].setAttribute("style", 'background-color:red;color:white;');
-        //document.querySelectorAll("#infocontent")[0].append(data.message);
+        document.querySelectorAll(".infoT")[0].setAttribute("style", 'background-color:red;color:white;');
+        document.querySelectorAll("#infocontnet")[0].innerHTML=data.message1 + "<br>" + "<br>" + data.message2;
+        document.querySelectorAll("#infoeng")[0].innerHTML=data.message3;
 
 });
