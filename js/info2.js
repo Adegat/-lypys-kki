85983304014dcc3ef58480a860b8de71563785ff
Adegat <adel.gatoui@gmail.com>
2017-11-28 11:03:54 +0200

    ruotsi info

--- js/info2.js
+++ js/info2.js
@@ -4,10 +4,10 @@ $.ajax({
     method: 'GET'
 }).then(function(data) {
     linebreak= document.createElement("br")
-    console.log(data,'moi');
         //console.log("danger");
         document.querySelectorAll(".infoT")[0].setAttribute("style", 'background-color:red;color:white;');
-        document.querySelectorAll("#infocontnet")[0].innerHTML=data.message1 + "<br>" + "<br>" + data.message2;
+        document.querySelectorAll("#infocontnet")[0].innerHTML=data.message1;
+        document.querySelectorAll("#infosv")[0].innerHTML=data.message2;
         document.querySelectorAll("#infoeng")[0].innerHTML=data.message3;
 
 });
