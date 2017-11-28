ca182641973bbd70b9ea520ef3423d9f5899d0ae
Adegat <adel.gatoui@gmail.com>
2017-11-28 13:12:26 +0200

    kommitti

--- js/info.js
+++ js/info.js
@@ -3,3 +3,17 @@ function myInfo() {
 	$("#info").leanModal();		
     //alert("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pretium, sem sit amet consequat porta, felis justo ullamcorper justo, et rhoncus neque neque quis dolor. Curabitur malesuada neque nec nunc maximus, vitae bibendum dui consequat. Fusce viverra metus sit amet nibh tempus suscipit. Proin lobortis nulla sem, vitae interdum arcu mollis id. Aliquam sit amet elit et lacus tincidunt commodo. Donec id pellentesque turpis, et sollicitudin turpis. Donec placerat enim in nulla ultrices placerat. Praesent dapibus lectus nisl, feugiat faucibus eros accumsan ac.");
 }
+
+$.ajax({
+    url: '../../../../data/config.json',
+    header: ('Content-Type: application/json; charset=utf-8'),
+    method: 'GET'
+}).then(function(data) {
+    linebreak= document.createElement("br")
+    console.log(data,'moi');
+    //console.log("danger");
+    document.querySelectorAll(".infoT")[0].setAttribute("style", 'background-color:purple;color:white;');
+    document.querySelectorAll("#infocontnet")[0].innerHTML = data.message1;
+    document.querySelectorAll("#infosv")[0].innerHTML = data.message2;
+    document.querySelectorAll("#infoeng")[0].innerHTML = data.message3;
+});
--- js/notices.js
+++ js/notices.js
@@ -7,14 +7,14 @@ $.ajax({
 		if (data.level == 1){
 			//console.log("neutral");
 			document.querySelectorAll(".notices")[0].setAttribute("style", "background-color:blue;");
-			document.querySelectorAll("#incl")[0].append(data.message);
+			document.querySelectorAll("#incl")[0].append(data.alert);
 		} else if (data.level == 2) {
 			//console.log("traffic");
 			document.querySelectorAll(".notices")[0].setAttribute("style", 'background-color:yellow;color:black;');
-			document.querySelectorAll("#incl")[0].append(data.message);
+			document.querySelectorAll("#incl")[0].append(data.alert);
 		} else {
 			//console.log("danger");
 			document.querySelectorAll(".notices")[0].setAttribute("style", 'background-color:red;color:white;');
-			document.querySelectorAll("#incl")[0].append(data.message);
+			document.querySelectorAll("#incl")[0].append(data.alert);
 		}
 });
