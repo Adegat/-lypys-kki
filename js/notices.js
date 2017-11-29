c6edcc96bb6695b5eb6ee693a1596ee5c1dda6e8
Hannes Tikkanen <tikkha1@gmail.com>
2017-11-29 13:20:52 +0200

    -

--- js/notices.js
+++ js/notices.js
@@ -5,16 +5,21 @@ $.ajax({
 	}).then(function(data) {
 		//console.log(data);
 		if (data.level == 1){
-			//console.log("neutral");
+			//Neutral notices
 			document.querySelectorAll(".notices")[0].setAttribute("style", "background-color:blue;");
 			document.querySelectorAll("#incl")[0].append(data.message);
 		} else if (data.level == 2) {
-			//console.log("traffic");
+			//Traffic notices
 			document.querySelectorAll(".notices")[0].setAttribute("style", 'background-color:yellow;color:black;');
 			document.querySelectorAll("#incl")[0].append(data.message);
 		} else {
-			//console.log("danger");
+			//Emergency notices
 			document.querySelectorAll(".notices")[0].setAttribute("style", 'background-color:red;color:white;');
 			document.querySelectorAll("#incl")[0].append(data.message);
 		}
+		
+		
+		
+		
+		
 });
