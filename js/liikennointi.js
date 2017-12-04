89982488639fa3b8b2c84169e729607f1f79f6fd
Adegat <adel.gatoui@gmail.com>
2017-12-04 11:14:20 +0200

    kiliä

--- js/liikennointi.js
+++ js/liikennointi.js
@@ -5,10 +5,10 @@ function liikennointi() {
     var time = d.toLocaleTimeString();
     //console.log(time)
     if(aikaam <= time && time <= aikapm){
-        document.getElementById("AikaTaulu").innerHTML = "<h4>Älybussi on liikenteessä</h4>" + " " + "<i class=\'fa fa-bus\' aria-hidden=\'true\'></i>"
+        document.getElementById("AikaTaulu").innerHTML = "<h4>Älybussi on liikenteessä/ Den smarta bussen är i trafik/ The smart bus is in traffic</h4>" + " " + "<i class=\'fa fa-bus\' aria-hidden=\'true\'></i>"
         document.getElementById("AikaTaulu").setAttribute("style", 'background-color:#4806B1');
     }else{
-        document.getElementById("AikaTaulu").innerHTML = "<h4>Älybussi ei liikennöi</h4>" + "" + "<i class=\'fa fa-bus\' aria-hidden=\'true\'></i>"
+        document.getElementById("AikaTaulu").innerHTML = "<h4>Älybussi ei liikennöi/ Den smarta bussen är intte i trafik/ The smart bus is not in traffic</h4>" + "" + "<i class=\'fa fa-bus\' aria-hidden=\'true\'></i>"
         document.getElementById("AikaTaulu").setAttribute("style", 'background-color:orange');
     }
 
